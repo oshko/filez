8fda9edd6921dc1a2c04fd128599b715986d71ac
oshko <oshko.mail@gmail.com>
2025-12-12 22:06:28 -0800

    db created. schema ready

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
--- TODO
+CREATE TABLE folders (
+    id serial PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE files (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    size integer NOT NULL,
+    forder_id integer REFERENCES folders(id)    
+);
