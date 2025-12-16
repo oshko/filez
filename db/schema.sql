e28f462f7025fbe0d6c96d540744451d162dcf40
oshko <oshko.mail@gmail.com>
2025-12-15 16:10:21 -0800

    Test passed for database

--- db/schema.sql
+++ db/schema.sql
@@ -11,5 +11,6 @@ CREATE TABLE files (
     id serial PRIMARY KEY,
     name text NOT NULL,
     size integer NOT NULL,
-    folder_id integer REFERENCES folders(id) ON DELETE CASCADE   
+    folder_id integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+    UNIQUE (name, folder_id)
 );
